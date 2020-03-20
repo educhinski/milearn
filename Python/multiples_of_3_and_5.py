07f6c2cc9f36a3d863c62987fc2e974825b26d35
educhinski <educhinski650@gmail.com>
2020-03-20 12:00:55 +0300

    edit filename

--- Python/multiples_of_3_and_5.py
+++
@@ -1,5 +0,0 @@
-# Find numbers that are multiples of 3 and 5 and finds their sum
-
-multiples = [i for i in range(1, 1000) if i % 3 == 0 or i % 5 == 0]
-sum_multiples = sum(multiples)
-print("sum =", sum_multiples)

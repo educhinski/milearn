c722e7e03a78ebe4d3964ee671edc15b0a4531d8
educhinski <educhinski650@gmail.com>
2020-03-20 16:44:27 +0300

    added a function and a unittest test file, and some little adjustments

--- Python/multiples_of_3_and_5.py
+++ Python/multiples_of_3_and_5.py
@@ -1,7 +1,7 @@
 # Find numbers that are multiples of 3 and 5 and finds their sum
 
-multiples = [i for i in range(1, 1000) if i % 3 == 0 or i % 5 == 0]
-sum_multiples = sum(multiples)
+my_multiples = [i for i in range(1, 1000) if i % 3 == 0 or i % 5 == 0]
+sum_multiples = sum(my_multiples)
 
 
 # use a function
@@ -10,7 +10,13 @@ def sum_multiples(range_max, multiples):
     Accepts a max range(int) and list of multiples.
     Returns the sum of all the multiples in the given range.
     """
-    multiples_list = list({item for item in range(1, range_max)
-                           for mul in multiples if item % mul == 0})
+    if type(range_max) not in [int, float]:
+        raise TypeError("The max range must be a non-negative real number.")
 
-    return sum(multiples_list)
+    if range_max < 0:
+        raise ValueError("The max range cannot be negative.")
+
+    multiples_set = {item for item in range(1, int(range_max))
+                     for mul in multiples if item % mul == 0}
+
+    return sum(multiples_set)

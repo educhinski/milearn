99ea0455c3a50b9f172b1c60c428f8a56f8cc8e8
educhinski <educhinski650@gmail.com>
2020-03-28 10:50:52 +0300

    Added Euler folder

---
+++ Python/Euler/multiples_of_3_and_5.py
@@ -0,0 +1,24 @@
+"""" 
+Find numbers that are multiples of the given values in a range
+and finds their sum
+"""
+my_multiples = [i for i in range(1, 1000) if i % 3 == 0 or i % 5 == 0]
+sum_multiples = sum(my_multiples)
+
+
+# use a function
+def sum_multiples(range_max, multiples):
+    """
+    Accepts a max range(int) and list of multiples.
+    Returns the sum of all the multiples in the given range.
+    """
+    if type(range_max) not in [int, float]:
+        raise TypeError("The max range must be a non-negative real number.")
+
+    if range_max < 0:
+        raise ValueError("The max range cannot be negative.")
+
+    multiples_set = {item for item in range(1, int(range_max))
+                     for mul in multiples if item % abs(mul) == 0}
+
+    return sum(multiples_set)

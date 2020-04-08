e56824b5f37b94415763b6d6248372ae59a4c818
educhinski <educhinski650@gmail.com>
2020-04-08 16:56:31 +0300

    improved fibonacci, adding some raises counterchecks

--- Python/Euler/fibonacci.py
+++ Python/Euler/fibonacci.py
@@ -1,27 +1,40 @@
 """Finds fibonacci numbers below a certain given value."""
 
 
-def fibonacci(end_range, start_values=(1, 2)):
-    
+def fibonacci(end, start=0):
     """
     Returns a list of fibonacci numbers below end_range
     starting from start_values.
 
     Variables:
-    end_range: Value which the fibonacci sequence should not exceed
-        Ex. end_range=10 returns [1, 2, 3, 5, 8]
-    start_values: default = (1, 2)
-        tuple to specify beginning of sequence
+    end_range: <int> Value which the fibonacci sequence should not exceed
+    start: <int> (optional) Value to specify where sequence should start.
+        default: 0
     """
 
+    if type(end) not in [int] or type(start) not in [int]:
+        raise TypeError("The end or start must be a non-negative real number")
+
+    if end < 1:
+        raise ValueError("The end value must be greater than 1")
+
+    if start < 0:
+        raise ValueError("The start value must be greater than 0")
+
+    a, b = start, start + 1
+    if end == start:
+        raise ValueError("The start and end cannot be equal!")
+
+    if end < b:
+        raise ValueError("End value must be greater than the start value")
+
     fib_list = []
-    a, b = start_values[0], start_values[1]
     fib_list.extend([a, b])
 
     # loop through the fibonacci sequence while less than end_range
-    while fib_list[-1] < end_range:
+    while b < end:
         a, b = b, a+b
-        if b > end_range:
+        if b > end:
             break
         fib_list.append(b)
 

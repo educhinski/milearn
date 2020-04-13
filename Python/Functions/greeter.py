dd5362d1017dc64f6cf34902dad8665e45a3b97f
educhinski <educhinski650@gmail.com>
2020-04-13 12:31:54 +0300

    modified function to take username

--- Python/Functions/greeter.py
+++ Python/Functions/greeter.py
@@ -1,10 +1,13 @@
 """a function that greets the user"""
 
 
-def greet_user():
-    """Display a simple greeting."""
+# passing info to a function
+def greet_user(username):
+    """Display a simple greeting to the user passed."""
 
-    print("Hello!")
+    # greets the user by their name, capitalized
+    print("Hello, " + username.title() + "!")
 
 
-greet_user()
+# now we pass the username to the function
+greet_user('jesse')

c7dabe255281bf52cc0ecf16b206d653df833854
educhinski <educhinski650@gmail.com>
2020-04-13 12:51:56 +0300

    added function to display message

---
+++ Python/Functions/message.py
@@ -0,0 +1,10 @@
+"""a function that displays a message"""
+
+
+def display_message(message):
+    """Displays the message passed, capitalized"""
+
+    print(message.capitalize())
+
+
+display_message("hey, I am writing code and pushing it to github!")

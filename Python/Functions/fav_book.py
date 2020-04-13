c7f9a838e8da755ce73b1301872b1b4e2ec05fac
educhinski <educhinski650@gmail.com>
2020-04-13 13:00:11 +0300

    added a function to display a book title

---
+++ Python/Functions/fav_book.py
@@ -0,0 +1,10 @@
+"""a function to display one's favorite book"""
+
+
+def favorite_book(book_title):
+    """Prints a nicely formatted description of the book"""
+
+    print("One of my favorite books is " + book_title.title() + ".")
+
+
+favorite_book('Alice in Wonderland')

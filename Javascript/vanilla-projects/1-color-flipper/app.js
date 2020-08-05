d22a6de88bc165a148dc491d8ee38b5326392315
educhinski <educhinski650@gmail.com>
2020-08-05 17:19:11 +0300

    Added js functionality for the simple color flipping, updated the necessary markup and styles

--- Javascript/vanilla-projects/1-color-flipper/app.js
+++ Javascript/vanilla-projects/1-color-flipper/app.js
@@ -1,1 +1,17 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+color.textContent = '#f1f5f8';
+
+btn.addEventListener('click', function () {
+    // get random number between 0 and length of array
+    const randomNumber = getRandomNumber();
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
